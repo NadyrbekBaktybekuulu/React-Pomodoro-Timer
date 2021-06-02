6238ef434037c6fbff0f23e0504137c0a0270b90
Nadyrbek Baktybek uulu <nadyrbekbaktybekuulu@gmail.com>
2021-06-02 13:18:26 +0600

    second commit

--- src/hook/usePageTitle.js
+++ src/hook/usePageTitle.js
@@ -29,7 +29,8 @@ function usePageTitle(time, mode) {
 
   if (mode === 'stopped' || mode === 'working') {
     pageBody.style.background = 
-    'linear-gradient(90deg, #F78CA0 0%, #F9748F 20.31%, #FD868C 66.67%, #FE9A8B 100%), #FFFFFF';
+    'linear-gradient(90deg, #F78CA0 0%, #F9748F' +
+     ' 20.31%, #FD868C 66.67%, #FE9A8B 100%), #FFFFFF';
   } else if (mode === 'bigBreak' || mode === 'smallBreak') {
     pageBody.style.background = 
     'linear-gradient(180deg, #48C6EF 0%, #6F86D6 100%)';
